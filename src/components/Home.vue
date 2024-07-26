8da4218533b7ec7def09f4db9caf11e2d8c38b1e
mahdi-fahimi <webdesign.fahimi79@gmail.com>
2024-07-26 16:27:33 +0430

    logic of increase font size btn and decrease font size btn added

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -24,16 +24,22 @@
     </div>
 
 <!--  btns  -->
-    <div class="absolute top-72 left-4 flex flex-col gap-2">
+<!--    <div class="absolute top-72 left-4 flex flex-col gap-2">-->
+    <div class="fixed top-72 left-4 flex flex-col gap-2">
       <Btn :severity="plusBtnProps.severity"
            :icon="plusBtnProps.icon"
            :raised="plusBtnProps.raised"
            :rounded="plusBtnProps.rounded"
+           @click="changeFontSize('+', 2)"
+           :title="arabicFontSize"
+
       />
       <Btn :severity="minusBtnProps.severity"
            :icon="minusBtnProps.icon"
            :raised="minusBtnProps.raised"
            :rounded="minusBtnProps.rounded"
+           @click="changeFontSize('-', 2)"
+           :title="arabicFontSize"
       />
     </div>
 <!--  aya and translation  -->
@@ -45,17 +51,19 @@
         </p>
       </div>
 <!--   بسم الله   -->
+<!--      <p class="arabic-font-size"> بسم الله الرحمان  الرحمیم </p>-->
+<!--      <p class="translation-font-size"> بسم الله الرحمان  الرحمیم </p>-->
       <div v-if="suraNumber !== 9 && !isSearching" class="text-center mt-8">
-        <p  class="uthmanTaha-font text-2xl mb-4">
+        <p  class="uthmanTaha-font mb-4 arabic-font-size">
           بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ
         </p>
-        <p v-if="translatorId === 1" class="text-lg mb-4">
+        <p v-if="translatorId === 1" class="mb-4 translation-font-size">
           In the name of Allah, Most Gracious, Most Merciful
         </p>
-        <p v-else-if="translatorId === 2" class="vazir-font text-lg mb-4">
+        <p v-else-if="translatorId === 2" class="vazir-font mb-4 translation-font-size">
           به نام خداوند بخشنده مهربان
         </p>
-        <p v-else class="vazir-font text-lg mb-4">
+        <p v-else class="vazir-font mb-4 translation-font-size">
           ستایش مخصوص خداوندی است که پروردگار جهانیان است
         </p>
         <Divider/>
@@ -64,7 +72,7 @@
       <div id="sura-text" v-for="(aya, index) in allAyaText" :key="index">
         <div v-if="aya !== 'بِسْمِ اللَّهِ الرَّحْمَـٰنِ الرَّحِيمِ' ">
           <div id="aya" >
-            <p  class="uthmanTaha-font text-2xl leading-[4rem] inline text-justify " >
+            <p  class="uthmanTaha-font arabic-font-size leading-[4rem] inline text-justify " >
               {{ aya }}
             </p>
             <div id="aya-number"
@@ -74,10 +82,10 @@
                    alt="bracket"
                    class="w-3"
               >
-              <span v-if="isSearching" class="uthmanTaha-font mx-1 text-xl">
+              <span v-if="isSearching" class="uthmanTaha-font mx-1 translation-font-size">
                {{ allSuraNameSearch[index] }}
               </span>
-              <span  class="vazir-font mx-1 text-lg">
+              <span  class="vazir-font mx-1 translation-font-size">
                 {{ allAyaNumber[index] }}
               </span>
               <img src="https://upload.wikimedia.org/wikipedia/commons/1/18/La_bracket.png"
@@ -94,12 +102,12 @@
           </div>
           <div id="translation">
             <p v-if="suraNumber === 1"
-               class="vazir-font text-lg leading-9 text-justify"
+               class="vazir-font translation-font-size leading-9 text-justify"
                :class="{'direction-ltr' : translatorId === 1  }">
               {{ allTranslation[index-1] }}
             </p>
             <p v-else
-               class="vazir-font text-lg leading-9 text-justify"
+               class="vazir-font translation-font-size leading-9 text-justify"
                :class="{'direction-ltr' : translatorId === 1}">
               {{ allTranslation[index] }}
             </p>
@@ -158,6 +166,12 @@ const selectTranslationProps = {
   optionLabel: 'translator',
   placeholder: 'انتخاب ترجمه'
 }
+
+const arabicFontSize = ref(24)
+const translationFontSize = ref(18)
+const translationFontSizeString = ref(`${translationFontSize.value}px`)
+const arabicFontSizeString = ref(`${arabicFontSize.value}px`)
+
 
 ///////////////////////////////
 
@@ -273,6 +287,18 @@ function getTranslation(value){
       })
 }
 
+function changeFontSize(operator , n){
+  if(operator === '+' && arabicFontSize.value < 50){
+    arabicFontSize.value += n;
+    translationFontSize.value += n;
+  }
+  if(operator === '-' && translationFontSize.value > 10){
+    arabicFontSize.value -= n;
+    translationFontSize.value -= n;
+  }
+  translationFontSizeString.value = `${translationFontSize.value}px`;
+  arabicFontSizeString.value =`${arabicFontSize.value}px`;
+}
 
 getAllSuraNames()
 
@@ -293,7 +319,11 @@ getAya(suraNumber.value)
   border-image: url(../assets/images/border2.png) 106 round;
 }
 
-#abc{
-  direction: ltr !important;
+.arabic-font-size{
+  font-size: v-bind(arabicFontSizeString);
 }
+.translation-font-size{
+  font-size: v-bind(translationFontSizeString);
+}
+
 </style>
